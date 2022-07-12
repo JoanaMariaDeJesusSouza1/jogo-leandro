af4d532166da470616729d4ced41b13f7f157400
joana maria de jesus souza <98822804+JoanaMariaDeJesusSouza1@users.noreply.github.com>
2022-07-12 19:04:46 -0300

    Add files via upload

--- jogo-leandro-main/PRO_C17_LP_AP1-main/sketch.js
+++ jogo-leandro-main/PRO_C17_LP_AP1-main/sketch.js
@@ -45,22 +45,22 @@ function setup(){
   ronaldo = createSprite(1000,600,100,100);
   ronaldo.addImage("ronaldo",ronaldoImg);
   ronaldo.scale = 0.4;
-  ronaldo.debug=true
-  ronaldo.setCollider("rectangle",0,0,50,50)
+  ronaldo.debug=false
+  ronaldo.setCollider("rectangle",0,0,180,120)
 
   ball = createSprite(1200,715,100,100);
   ball.addImage("ball",ballImg);
   ball.scale = 0.4;
   ball.visible = false;
-  
+  ball.setCollider("circle",0,0,50)
 
   buraco = createSprite(300,270,10,10);
   buraco.addImage("buraco",buracoImg);
   buraco.scale = 0.6;
   buraco.depth = ronaldo.depth-1;
   buraco.visible = true;
-  buraco.debug=true
-  buraco.setCollider("rectangle",0,0,50,50)
+  buraco.debug=false
+  buraco.setCollider("rectangle",0,0,100,100)
 
   buraco2 = createSprite(1600,850,10,10);
   buraco2.addImage("buraco",buracoImg);
@@ -168,12 +168,47 @@ function draw(){
     ronaldo.x+=5;
   }
 
+  
+  ronaldo.bounce(ground)
+  ronaldo.bounce(ground2)
+  ronaldo.bounce(ground3)
+  ronaldo.bounce(ground4)
+  ronaldo.bounce(ground5)
+  ronaldo.bounce(ground6)
+  ronaldo.bounce(ground7)
+  ronaldo.bounce(ground8)
+  ronaldo.bounce(ground9)
+  ronaldo.bounce(ground10)
+  ronaldo.bounce(ground11)
+  ronaldo.bounce(ground12)
+  ronaldo.bounce(ground13)
+  ronaldo.bounce(ground14)
+  ronaldo.bounce(ground15)
+  ronaldo.bounce(ground16)
+  ronaldo.bounce(ground17)
+  ronaldo.bounce(ground18)
+  ronaldo.bounce(ground19)
+  ronaldo.bounce(ground20)
+  ronaldo.bounce(ground21)
+  ronaldo.bounce(ground22)
+  ronaldo.bounce(ground23)
+  ronaldo.bounce(ground24)
+  ronaldo.bounce(ground25)
+  ronaldo.bounce(ground26)
+  ronaldo.bounce(ground27)
+  ronaldo.bounce(ground28)
+  ronaldo.bounce(ground29)
+  ronaldo.bounce(ground30)
+ 
 
   if(ronaldo.isTouching(buraco)){
-   background.visible=false;
+ 
+   ronaldo.scale=0.2;
    crianca.visible=false;
    buraco2.visible=true;
    ball.visible=true;
+   ronaldo.setCollider("rectangle",0,0,180,300)
+
    ground.visible=true;
    ground2.visible=true;
    ground3.visible=true;
@@ -205,12 +240,96 @@ function draw(){
    ground29.visible=true;
    ground30.visible=true;
    ground31.visible=true;
+
+  //  ronaldo.bounceOff(ground)
+  //  ronaldo.bounce(ground2)
+  //  ronaldo.bounce(ground3)
+  //  ronaldo.bounce(ground4)
+  //  ronaldo.bounce(ground5)
+  //  ronaldo.bounce(ground6)
+  //  ronaldo.bounce(ground7)
+  //  ronaldo.bounce(ground8)
+  //  ronaldo.bounce(ground9)
+  //  ronaldo.bounce(ground10)
+  //  ronaldo.bounce(ground11)
+  //  ronaldo.bounce(ground12)
+  //  ronaldo.bounce(ground13)
+  //  ronaldo.bounce(ground14)
+  //  ronaldo.bounce(ground15)
+  //  ronaldo.bounce(ground16)
+  //  ronaldo.bounce(ground17)
+  //  ronaldo.bounce(ground18)
+  //  ronaldo.bounce(ground19)
+  //  ronaldo.bounce(ground20)
+  //  ronaldo.bounce(ground21)
+  //  ronaldo.bounce(ground22)
+  //  ronaldo.bounce(ground23)
+  //  ronaldo.bounce(ground24)
+  //  ronaldo.bounce(ground25)
+  //  ronaldo.bounce(ground26)
+  //  ronaldo.bounce(ground27)
+  //  ronaldo.bounce(ground28)
+  //  ronaldo.bounce(ground29)
+  //  ronaldo.bounce(ground30)
+  //  ronaldo.bounce(ground31)
+
+  
   }
 
+  if(ronaldo.isTouching(ball)){
+    textAlign(CENTER,TOP)
+    textSize(50)
+    text("agora saia do labirinto",900,400)
+    setTimeout(()=>{
+      ball.visible=false
+      ground31.visible=false
+     
+    },5000)
+   
+  }
+  if(ronaldo.isTouching(buraco2)){
+   
+    crianca.visible=true;
+    buraco2.visible=true;
+    ball.visible=false;
+    ronaldo.setCollider("rectangle",0,0,180,30)
+    ground.visible=false;
+    ground2.visible=false;
+    ground3.visible=false;
+    ground4.visible=false;
+    ground5.visible=false;
+    ground6.visible=false;
+    ground7.visible=false;
+    ground8.visible=false;
+    ground9.visible=false;
+    ground10.visible=false;
+    ground11.visible=false;
+    ground12.visible=false;
+    ground13.visible=false;
+    ground14.visible=false;
+    ground15.visible=false;
+    ground16.visible=false;
+    ground17.visible=false;
+    ground18.visible=false;
+    ground19.visible=false;
+    ground20.visible=false;
+    ground21.visible=false;
+    ground22.visible=false;
+    ground23.visible=false;
+    ground24.visible=false;
+    ground25.visible=false;
+    ground26.visible=false;
+    ground27.visible=false;
+    ground28.visible=false;
+    ground29.visible=false;
+    ground30.visible=false;
+    ground31.visible=false;
+  }
  
    drawSprites();
   }
   
   function mousePressed(){
   
+
   }
